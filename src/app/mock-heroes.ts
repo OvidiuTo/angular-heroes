42bfca360425a4d41775edfaecbf7e8059286bfd
Ovidiu <ovidiutoma876@gmail.com>
2020-08-01 11:15:55 +0300

    Made a Display List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id : 11, name: 'Paladin' },
+    { id : 12, name: 'Pirate' },
+    { id : 13, name: 'Nomad' },
+    { id : 14, name: 'Valkyrae' },
+    { id : 15, name: 'Viking' },
+    { id : 16, name: 'Pyromancer' },
+    { id : 17, name: 'Samurai' },
+    { id : 18, name: 'Plague Doctor' },
+    { id : 19, name: 'Shield Lancer' },
+    { id : 20, name: 'Necromancer' },
+
+];
