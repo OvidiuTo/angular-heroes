bbbc3dff6ebc71c6dc2455ca03b5603fb2ae2b95
Ovidiu <ovidiutoma876@gmail.com>
2020-08-04 01:15:26 +0300

    CSS Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,7 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
+    
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
